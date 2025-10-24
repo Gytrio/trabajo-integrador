4a10bf625c81076ab8412c7eb2d1b575fe9669d7
Maximiliano Caneda <maxicaneda45@gmail.com>
2025-10-24 03:20:25 -0300

    Agrego transicion de fondo con color segun categoria

--- trabajo-integrador/src/App.jsx
+++ trabajo-integrador/src/App.jsx
@@ -1,27 +1,24 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // <-- FALTABA
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Nav } from "./components/Nav/Nav.jsx";
+import Layout from "./components/Layout/Layout.jsx";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer.jsx";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer.jsx";
 import { CartProvider } from "./Context/CartContext/CartProvider.jsx";
-function App() {
 
-  return (
-    <>
+export default function App() {
+    return (
         <BrowserRouter>
             <CartProvider>
-                
-            <Nav/>
-            <Routes>
-                <Route path="/" element={<ItemListContainer />} />
-                <Route path="/category/:platform" element={<ItemListContainer />} />
-                <Route path="/details/:id" element={<ItemDetailContainer />} />
-            </Routes>
+                <Nav />
+                <Routes>
+                        <Route element={<Layout />}>
+                        <Route path="/" element={<ItemListContainer />} />
+                        <Route path="/category/:platform" element={<ItemListContainer />} />
+                        <Route path="/details/:id" element={<ItemDetailContainer />} />
+                    </Route>
+                </Routes>
             </CartProvider>
-
         </BrowserRouter>
-    </>
-  )
+    );
 }
-
-export default App
